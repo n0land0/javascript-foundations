69c3ffd9c4241cfaf4026e7e62bf929cc5681bc5
Nolan Caine <nolancaine@gmail.com>
2021-07-12 19:19:23 -0600

    Add dragon.js and dragon-test.js

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,25 @@
+class Dragon {
+  constructor(name, rider) {
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+    this.mealCounter = 0;
+  }
+  greet() {
+    return `Hi, ${this.rider}!`;
+  }
+  eat() {
+    // for (var mealCounter = 0; mealCounter < 4; mealCounter++) {
+    //   mealCounter+= 1;
+    //   if (mealCounter >= 3) {
+    //     return this.hungry = false;
+    //   }
+    // }
+    this.mealCounter += 1;
+    if (this.mealCounter >= 3) {
+      this.hungry = false;
+    }
+  }
+}
 
+module.exports = Dragon;
