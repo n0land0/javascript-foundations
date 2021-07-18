c14da092ea1a3f79f021ef170dbb1138b1532b62
Nolan Caine <nolancaine@gmail.com>
2021-07-17 22:34:09 -0600

    Add medusa, person, statue, and medusa-test and update readme

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,23 @@
+class Medusa {
+  constructor(name) {
+    this.name = name;
+    this.statues = [];
+  }
+  gazeAtVictim(victimObj) {
+    var personToStatue = new Statue(victimObj.name);
+    this.statues.push(personToStatue);
+    if (this.statues.length > 3) {
+      // this.statues[0] = new Person(this.statues[0].name);
+      var statueToPerson = new Person(this.statues[0].name);
+      statueToPerson.mood = "relieved";
+      console.log(this.statues[0]);
+      this.statues.shift();
+      return statueToPerson;
+    }
+  };
+};
+
 var Person = require('./person');
 var Statue = require('./statue');
+
+module.exports = Medusa;
