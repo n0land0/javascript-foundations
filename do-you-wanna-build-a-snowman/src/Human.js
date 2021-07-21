5af4481974d73dbcb7d4779e22cd714109fc974a
Nolan Caine <nolancaine@gmail.com>
2021-07-21 09:55:17 -0600

    Add snowman and human

--- do-you-wanna-build-a-snowman/src/Human.js
+++ do-you-wanna-build-a-snowman/src/Human.js
@@ -1,7 +1,33 @@
 var Snowman = require('./Snowman');
 
 class Human {
-
+  constructor(name) {
+    this.name = name
+    this.wantsToBuildASnowman = true;
+    this.materials = {
+      snowballs: 0,
+      coal: 0,
+      buttons: 0,
+      carrots: 0
+    }
+  }
+  gatherMaterials(materialsProperty, quantity) {
+    if (this.materials.hasOwnProperty(materialsProperty)) {
+      this.materials[materialsProperty] += quantity;
+    }
+  }
+  buildASnowman() {
+    return new Snowman(this.materials);
+  }
+  placeMagicHat() {
+    var snowman = this.buildASnowman();
+    snowman.canWearMagicHat();
+    if (snowman.magicHat) {
+      return "Woah, this snowman is coming to life!";
+    } else {
+      return "I guess I didn\'t build it correctly.";
+    }
+  }
 }
 
 module.exports = Human;
