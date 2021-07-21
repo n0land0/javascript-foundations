4e52e339e80ab9cbdf01a04b5d01bdfd128bcfb4
Nolan Caine <nolancaine@gmail.com>
2021-07-21 00:06:15 -0600

    Add skier and lift

--- ski-lift/src/Lift.js
+++ ski-lift/src/Lift.js
@@ -1,7 +1,16 @@
 var Skier = require('./Skier');
 
 class Lift {
-  
+  constructor(limit) {
+    this.inService = true;
+    this.limit = limit;
+    this.skiers = [];
+    this.safetyBar = "up";
+  }
+  admitSkier(name, hasLiftTicket) {
+    var skier = new Skier(name, hasLiftTicket);
+    this.skiers.push(skier);
+  }
 }
 
 module.exports = Lift;
